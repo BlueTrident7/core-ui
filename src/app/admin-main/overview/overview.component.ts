fc2542055e65f8e540a63279fdc5ae37233c2afe
kakarla.nb <kakarla.nb@lifetrenz.com>
2025-10-04 18:30:17 +0530

    Admin

--- src/app/admin-main/overview/overview.component.ts
+++ src/app/admin-main/overview/overview.component.ts
@@ -1,30 +1,42 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    CardModule,
+    TableModule,
+    CheckboxModule,
+    DropdownModule,
+    ButtonModule,
+  ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css',
 })
 export class OverviewComponent {
-  users = [
-    { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'User' },
-    { id: 3, name: 'Bob Johnson', email: 'bob@example.com', role: 'User' },
-  ];
-
-  stats = {
-    totalUsers: 150,
-    activeSessions: 45,
-    totalTransactions: 1200,
-  };
-
-  settings = {
-    notifications: true,
-    twoFactor: false,
-    theme: 'dark',
+  payload = {
+    stats: {
+      totalUsers: 150,
+      activeSessions: 45,
+      totalTransactions: 1200,
+    },
+    users: [
+      { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Admin' },
+      { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'User' },
+    ],
+    settings: {
+      notifications: true,
+      twoFactor: false,
+      theme: 'dark',
+    },
   };
 }
